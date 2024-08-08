12bc27387daa9a3ea49cf085955dcdc8c88dea26
mfarhad1994 <104850206+mfarhad1994@users.noreply.github.com>
2024-08-08 14:31:25 +0000

    llm

---
+++ local_elastic_flan-t5-small_qa_faq.py
@@ -0,0 +1,103 @@
+import streamlit as st
+import time
+
+from elasticsearch import Elasticsearch
+from openai import OpenAI
+
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-small")
+model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-small")
+
+es_client = Elasticsearch('http://localhost:9200') 
+
+
+def elastic_search(query, index_name = "course-questions"):
+    search_query = {
+        "size": 5,
+        "query": {
+            "bool": {
+                "must": {
+                    "multi_match": {
+                        "query": query,
+                        "fields": ["question^3", "text", "section"],
+                        "type": "best_fields"
+                    }
+                },
+                "filter": {
+                    "term": {
+                        "course": "data-engineering-zoomcamp"
+                    }
+                }
+            }
+        }
+    }
+
+    response = es_client.search(index=index_name, body=search_query)
+    
+    result_docs = []
+    
+    for hit in response['hits']['hits']:
+        result_docs.append(hit['_source'])
+    
+    return result_docs
+
+
+def build_prompt(query, search_results):
+    prompt_template = """
+You're a course teaching assistant. Answer the QUESTION based on the CONTEXT from the FAQ database.
+Use only the facts from the CONTEXT when answering the QUESTION.
+
+QUESTION: {question}
+
+CONTEXT: 
+{context}
+""".strip()
+
+    context = ""
+    
+    for doc in search_results:
+        context = context + f"section: {doc['section']}\nquestion: {doc['question']}\nanswer: {doc['text']}\n\n"
+    
+    prompt = prompt_template.format(question=query, context=context).strip()
+    return prompt
+
+
+
+def llm(prompt, generate_params=None):
+    if generate_params is None:
+        generate_params = {}
+
+    input_ids = tokenizer(prompt, return_tensors="pt").input_ids#.to("cuda")
+    outputs = model.generate(
+        input_ids,
+        max_length=generate_params.get("max_length", 100),
+        num_beams=generate_params.get("num_beams", 5),
+        do_sample=generate_params.get("do_sample", False),
+        temperature=generate_params.get("temperature", 1.0),
+        top_k=generate_params.get("top_k", 50),
+        top_p=generate_params.get("top_p", 0.95),
+    )
+    result = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return result
+
+
+def rag(query):
+    search_results = elastic_search(query)
+    prompt = build_prompt(query, search_results)
+    answer = llm(prompt)
+    return answer
+
+
+def main():
+    st.title("RAG Function Invocation")
+
+    user_input = st.text_input("Enter your input:")
+
+    if st.button("Ask"):
+        with st.spinner('Processing...'):
+            output = rag(user_input)
+            st.success("Completed!")
+            st.write(output)
+
+if __name__ == "__main__":
+    main()
